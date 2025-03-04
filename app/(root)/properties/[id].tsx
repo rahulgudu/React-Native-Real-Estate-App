e68a6d91ffb7bc09d59f6b11ff56ce17c03450d9
sushantskb <sushantsrk2001@gmail.com>
2025-03-04 11:24:35 +0530

    Added the property details page

--- app/(root)/properties/[id].tsx
+++ app/(root)/properties/[id].tsx
@@ -1,13 +1,36 @@
-import { View, Text } from "react-native";
+import ImageSlider from "@/components/ImageSlider";
+import PropertyDetails from "@/components/PropertyDetails";
+import icons from "@/constants/icons";
+import { router, useLocalSearchParams } from "expo-router";
 import React from "react";
-import { useLocalSearchParams } from "expo-router";
+import { Image, ScrollView, TouchableOpacity, View } from "react-native";
 
 const Property = () => {
   const { id } = useLocalSearchParams();
   return (
-    <View>
-      <Text>Property {id}</Text>
-    </View>
+    <ScrollView className="relative flex-1 bg-white">
+      <ImageSlider />
+      <View className="absolute top-2 left-5 w-full px-2 flex flex-row justify-between items-center">
+        <TouchableOpacity onPress={() => router.push("/")}>
+          <Image
+            source={icons.backArrow}
+            className="size-8 mix-blend-multiply"
+          />
+        </TouchableOpacity>
+
+        <View className="flex-row gap-2 mr-8">
+          <TouchableOpacity>
+            <Image source={icons.heart} className="size-6" tintColor="black" />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image source={icons.send} className="size-6" tintColor="black" />
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      {/* --Property Details-- */}
+      <PropertyDetails />
+    </ScrollView>
   );
 };
 
